22047698e474af63efe466e2184e67c617372869
Ashish <ashish.ashi6388@gmail.com>
2022-06-24 10:55:32 +0530

    Basic Logging Programe

---
+++ logging_1.py
@@ -0,0 +1,18 @@
+import logging
+
+logging.basicConfig()
+'''
+logging.debug("This is a debug message")
+logging.info("This is a info message")
+logging.warning("This is a warning")
+logging.error("This is an error")
+logging.critical("This is highly critical")
+
+Output:
+WARNING:root:This is a warning
+ERROR:root:This is an error
+CRITICAL:root:This is highly critical
+'''
+
+
+
